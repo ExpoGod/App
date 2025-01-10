7d16fcba56566e917946a3e62b2dea20ecb82323
DamianCMD <daga0300@gmail.com>
2025-01-10 17:16:40 -0600

    Login

--- Screens/Login.js
+++ Screens/Login.js
@@ -3,11 +3,14 @@ import {
   SafeAreaView,
   View,
   Text,
-  Button,
   TextInput,
   StyleSheet,
   Modal,
-  ActivityIndicator
+  ActivityIndicator,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  Platform
 } from "react-native";
 import * as SecureStore from 'expo-secure-store';
 
@@ -16,7 +19,7 @@ function Login({ navigation }) {
   const [claveDependencia, setClaveDependencia] = useState("");
   const [idTablet, setIdTablet] = useState("");
   const [claveSecreta, setClaveSecreta] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const ip = 'http://192.168.3.9:8080/';
 
   const handleLogin = async () => {
@@ -108,52 +111,81 @@ function Login({ navigation }) {
   }
 
   useEffect(() => {
-    checkToken();
+    //checkToken();
+    navigation.replace("Home");
     return () => {
 
     }
   }, []);
   return (
     <SafeAreaView style={styles.container}>
-      {/* Modal de carga */}
-      <Modal
-        visible={loading}
-        transparent={true}       // Para que se vea un fondo semi-transparente
-        animationType="none"    // Quita la animación del modal si lo deseas
-      >
-        <View style={styles.modalBackground}>
-          <View style={styles.activityIndicatorWrapper}>
-            <ActivityIndicator size="large" color="#333" />
-            <Text style={{ marginTop: 10 }}>Cargando...</Text>
+      <KeyboardAvoidingView style={styles.keyboard}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 0}>
+
+        {/* Modal de carga */}
+        <Modal
+          visible={loading}
+          transparent={true}       // Para que se vea un fondo semi-transparente
+          animationType="none"    // Quita la animación del modal si lo deseas
+        >
+          <View style={styles.modalBackground}>
+            <View style={styles.activityIndicatorWrapper}>
+              <ActivityIndicator size="large" color="#333" />
+              <Text style={{ marginTop: 10 }}>Cargando...</Text>
+            </View>
           </View>
+        </Modal>
+
+        <Image
+          source={require("../Assets/Images/Blue.png")}
+          style={styles.blueShape}
+        />
+
+        <Image
+          source={require("../Assets/Images/Purple.png")}
+          style={styles.purpleShape}
+        />
+
+        <Text style={styles.loginText}>Iniciar Sesión</Text>
+
+
+        <Image
+          source={require("../Assets/Images/Logo.png")}
+          style={styles.logo}
+        />
+        <Text style={styles.logoText}>A la Mano</Text>
+
+
+        <View style={styles.formContainer}>
+          <View style={styles.form}>
+            <Text style={styles.inputText} >
+              Clave Dependencia
+            </Text>
+            <TextInput
+              style={styles.input}
+              value={claveDependencia}
+              onChangeText={setClaveDependencia}
+            />
+            <Text style={styles.inputText} >ID Tablet</Text>
+            <TextInput
+              style={styles.input}
+              value={idTablet}
+              onChangeText={setIdTablet}
+            />
+            <Text style={styles.inputText}>Clave Secreta</Text>
+            <TextInput
+              style={styles.input}
+              value={claveSecreta}
+              onChangeText={setClaveSecreta}
+              secureTextEntry
+            />
+          </View>
+          <TouchableOpacity onPress={handleLogin}>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </TouchableOpacity>
         </View>
-      </Modal>
-
-      <Text style={styles.title}>Iniciar Sesión</Text>
-
-      <TextInput
-        style={styles.input}
-        placeholder="Clave Dependencia"
-        value={claveDependencia}
-        onChangeText={setClaveDependencia}
-      />
-
-      <TextInput
-        style={styles.input}
-        placeholder="ID Tablet"
-        value={idTablet}
-        onChangeText={setIdTablet}
-      />
-
-      <TextInput
-        style={styles.input}
-        placeholder="Clave Secreta"
-        value={claveSecreta}
-        onChangeText={setClaveSecreta}
-        secureTextEntry
-      />
-
-      <Button title="Entrar" onPress={handleLogin} />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -161,21 +193,15 @@ function Login({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 22,
-    marginBottom: 20,
-    textAlign: 'center'
+    position: "relative"
   },
   input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
     marginBottom: 15,
     paddingHorizontal: 10,
     paddingVertical: 8,
     borderRadius: 5,
+    marginBottom: 50,
+    backgroundColor: "white"
   },
   // Estilos para el modal
   modalBackground: {
@@ -190,6 +216,85 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     alignItems: 'center',
   },
+  formContainer:
+  {
+    position: "absolute",
+    top: 950,
+    width: "100%",
+    height: "auto",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  form: {
+    width: 600,
+  },
+  buttonText: {
+    position: "relative",
+    display: "flex",
+    paddingTop: 10,
+    paddingLeft: 70,
+    justifyContent: "center",
+    alignItems: 'center',
+    width: 300,
+    height: "auto",
+    fontSize: 50,
+    fontWeight: "600",
+    color: "white",
+    backgroundColor: "#12122E",
+    borderRadius: 10,
+    fontFamily: "Poppins_600SemiBold",
+  },
+  inputText: {
+    fontSize: 20,
+    paddingLeft: 20,
+    color: "#F5FFFA",
+    fontFamily: "Poppins_500Medium",
+  },
+  purpleShape: {
+    position: "absolute",
+    width: 1000,
+    height: 915,
+    top: 700,
+    resizeMode: 'contain',
+  },
+  blueShape: {
+    position: "absolute",
+    width: 700,
+    height: 400,
+    top: -81,
+    resizeMode: 'contain',
+  },
+  loginText: {
+    position: "absolute",
+    top: 40,
+    left: 60,
+    fontSize: 60,
+    color: "#F5FFFA",
+    fontFamily: "Poppins_700Bold"
+  },
+  logo: {
+    position: "absolute",
+    top: 400,
+    left: 300,
+    width: 400,
+    height: 400,
+    resizeMode: 'contain',
+  },
+  logoText: {
+    position: "absolute",
+    top: 800,
+    left: 350,
+    fontSize: 60,
+    color: "#F5FFFA",
+    fontFamily: "Poppins_700Bold"
+  },
+  keyboard: {
+    flex: 1,
+    position: "relative",
+    width: "100%",
+    height: "100%",
+  }
 });
 
 export default Login;
